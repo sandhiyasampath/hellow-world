f56b4a10ee5b6b2d65f03d193a0d90bfbe9c2754
sandhiyasampath <anbu171298@gmail.com>
2021-05-20 19:47:35 +0530

    accepting parameter from parent component

--- src/App.js
+++ src/App.js
@@ -7,30 +7,35 @@ import Message from './components/Message'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent'
+import UserGreeting from './components/UserGreeting'
 
 
 function App() {
   return (
     <div className="App">
-    {/*  <Hello /> */}
+      {/*  <Hello /> */}
 
-    {/*}  <Greet name = "Sandhiya" designation = "Developer"/>
+      {/*}  <Greet name = "Sandhiya" designation = "Developer"/>
         <p>This is a children part</p>
 
      {/* <Greet name = "Abhinaya" designation = "house wife"/>
       <Greet name = "Abirami" designation = "anaylists">
         <button>submit</button>
       </Greet>
-
-      <Welcome name = "Sandhiya" designation = "Developer"/>
+     <Welcome name = "Sandhiya" designation = "Developer"/>
       <Welcome name = "Abhinaya" designation = "house wife"/>
        <Message /> */}
 
-   {/* <Welcome name = "Abirami" designation = "anaylists"/>  */}
-    
-   {/* <Counter /> */}
-     {/*  <FunctionClick /> */}
-     <ClassClick />
+      {/* <Welcome name = "Abirami" designation = "anaylists"/>  */}
+
+      {/* <Counter /> */}
+      {/*  <FunctionClick /> */}
+      {/* <ClassClick /> */}
+      {/* <EventBind /> */}
+      {/*<ParentComponent />*/}
+      <UserGreeting />
     </div>
   );
 }
