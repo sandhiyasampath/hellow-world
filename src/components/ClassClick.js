eb25e51405e2651aaf5cb9860922d7c2fce40166
sandhiyasampath <anbu171298@gmail.com>
2021-05-20 19:49:13 +0530

    event demo

--- src/components/ClassClick.js
+++ src/components/ClassClick.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export class ClassClick extends Component {
-    clickHandler(){
+    clickHandler() {
         alert("clicked");
         console.log("button clicked")
     }
